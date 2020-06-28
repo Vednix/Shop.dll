1af635844b658fe1190d7cde96d72f150d526af8
Vednix <contato@terrariabrasil.com.br>
2020-06-28 13:16:11 -0300

    Do Release
    Tested with SEconomy 21

--- English/Shop.cs
+++ English/Shop.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using System.Collections.Generic;
 using TShockAPI;
 using Terraria;
@@ -11,17 +10,19 @@ using Mono.Data.Sqlite;
 using MySql.Data.MySqlClient;
 using TShockAPI.DB;
 using System.Data;
+using Microsoft.Xna.Framework;
+using Wolfje.Plugins.SEconomy.Journal;
 
 namespace Shop
 {
 	[ApiVersion(2, 1)]
 	public class Shop : TerrariaPlugin
 	{
-        public static IDbConnection ShopDB;
-        private Config config;
+		public static IDbConnection ShopDB;
+		private Config config;
 		public override Version Version
 		{
-			get { return new Version("1.3.0.1"); }
+			get { return new Version("1.3.2.0"); }
 		}
 		public override string Name
 		{
@@ -29,7 +30,7 @@ namespace Shop
 		}
 		public override string Author
 		{
-			get { return "RYH"; }
+			get { return "RYH & Little Update/Mobile port by Vednix"; }
 		}
 		public override string Description
 		{
@@ -41,184 +42,162 @@ namespace Shop
 		}
 		public override void Initialize()
 		{
-			TShockAPI.Commands.ChatCommands.Add(new Command(Buy, "buy"));
-			TShockAPI.Commands.ChatCommands.Add(new Command("shop.reload", ReloadConfig, "reloadshop"));
-            ReadConfig();
-		
+			Commands.ChatCommands.Add(new Command(Buy, "shop", "buy"));
+			Commands.ChatCommands.Add(new Command("shop.reload", ReloadConfig, "reloadshop"));
+			ReadConfig();
 
- 
-
-       
-            switch (TShock.Config.StorageType.ToLower())
-            {
-                case "mysql":
-                    string[] host = TShock.Config.MySqlHost.Split(':');
-                    ShopDB = new MySqlConnection()
-                    {
-                        ConnectionString = string.Format("Server={0}; Port={1}; Database={2}; Uid={3}; Pwd={4};",
-                            host[0],
-                            host.Length == 1 ? "3306" : host[1],
-                            TShock.Config.MySqlDbName,
-                            TShock.Config.MySqlUsername,
-                            TShock.Config.MySqlPassword)
-                    };
-                    break;
-                case "sqlite":
-                    string sql = Path.Combine(TShock.SavePath, "ShopHistory.sqlite");
-                    ShopDB = new SqliteConnection(string.Format("uri=file://{0},Version=3", sql));
-                    break;
-            }
-            SqlTableCreator sqlcreator = new SqlTableCreator(ShopDB,
-                ShopDB.GetSqlType() == SqlType.Sqlite ? (IQueryBuilder)new SqliteQueryCreator() : new MysqlQueryCreator());
-            sqlcreator.EnsureTableStructure(new SqlTable("ShopHistory",
-                new SqlColumn("Time", MySqlDbType.Int32),
-                new SqlColumn("Account", MySqlDbType.VarChar) { Length = 50 },
-                new SqlColumn("ItemName", MySqlDbType.String, 70),
-                new SqlColumn("WorldID", MySqlDbType.Int32),
-                new SqlColumn("Price", MySqlDbType.String, 100)));
-        }
-
-        
-
-
-        private static string ItemToTag(SimpleItem args)
+			switch (TShock.Config.StorageType.ToLower())
+			{
+				case "mysql":
+					string[] host = TShock.Config.MySqlHost.Split(':');
+					ShopDB = new MySqlConnection()
+					{
+						ConnectionString = string.Format("Server={0}; Port={1}; Database={2}; Uid={3}; Pwd={4};",
+							host[0],
+							host.Length == 1 ? "3306" : host[1],
+							TShock.Config.MySqlDbName,
+							TShock.Config.MySqlUsername,
+							TShock.Config.MySqlPassword)
+					};
+					break;
+				case "sqlite":
+					string sql = Path.Combine(TShock.SavePath, "ShopHistory.sqlite");
+					ShopDB = new SqliteConnection(string.Format("uri=file://{0},Version=3", sql));
+					break;
+			}
+			SqlTableCreator sqlcreator = new SqlTableCreator(ShopDB,
+				ShopDB.GetSqlType() == SqlType.Sqlite ? (IQueryBuilder)new SqliteQueryCreator() : new MysqlQueryCreator());
+			sqlcreator.EnsureTableStructure(new SqlTable("ShopHistory",
+				new SqlColumn("Time", MySqlDbType.Int32),
+				new SqlColumn("Account", MySqlDbType.VarChar) { Length = 50 },
+				new SqlColumn("ItemName", MySqlDbType.String, 70),
+				new SqlColumn("WorldID", MySqlDbType.Int32),
+				new SqlColumn("Price", MySqlDbType.String, 100)));
+		}
+		private static string ItemToTag(SimpleItem args)
 		{
-			string ret = ((args.prefix != 0) ? "[i/p" +args.prefix : "[i");
+			string ret = ((args.prefix != 0) ? "[i/p" + args.prefix : "[i");
 			ret = (args.stack != 1) ? ret + "/s" + args.stack : ret;
 			ret = ret + ":" + args.netID + "]";
 			if (args.netID == 0) return "";
 			return ret;
 		}
-        private void Buy(CommandArgs args)
-        {
-            if ((args.Parameters.Count < 1))
-            {
-                args.Player.SendErrorMessage("Check out our Shop, use: /buy name or /buy menu");
-                return;
-            }
-            if (args.Parameters[0] == "menu")
-            {
-                int pageNumber = 1;
-                if (!PaginationTools.TryParsePageNumber(args.Parameters, 1, args.Player, out pageNumber))
-                    return;
-                var lines = new List<string> { };
-                foreach (var gooda in config.All)
-                {
-                    string total = "* " + gooda.DisplayName + " - ";
-                    foreach (var item in gooda.IncludeItems)
-                    {
-                        total = total + ItemToTag(item) + " - " + Wolfje.Plugins.SEconomy.Money.Parse(Convert.ToString(gooda.Price));
-                    }
-                    if (args.Player.Group.HasPermission(gooda.RequirePermission))
-                    {
-                        lines.Add(total);
-                    }
-                    else if (!config.HideUnavailableGoods)
-                    {
-                        string perm = args.Player.Group.HasPermission(gooda.RequirePermission) ? "[Available]" : "[Shortage]";
-                        lines.Add(perm + total);
-                    }
-                }
-                PaginationTools.SendPage(args.Player, pageNumber, lines,
-                                         new PaginationTools.Settings
-                                         {
-                                             HeaderFormat = "Menu ({0}/{1}):",
-                                             FooterFormat = "Type {0}buy menu {{0}} for more goods.".SFormat(Commands.Specifier),
-                                             MaxLinesPerPage = 9
-                                         }
-                                        );
-                return;
-            }
-            var Find = new Goods();
-            bool FindSuccess = false;
-            int number = args.Parameters.Count;
-            string medname = args.Parameters[0];
-            for (int i = 1; i < number; i++)
-            {
-                medname = medname + " "+ args.Parameters[i];
+		private void Buy(CommandArgs args)
+		{
+			if ((args.Parameters.Count < 1))
+			{
+				args.Player.SendErrorMessage("Check out our Shop, use: [c/ffd700:/buy name] or [c/ffd700:/shop menu]");
+				return;
+			}
+			if (args.Parameters[0] == "menu")
+			{
+				int pageNumber = 1;
+				if (!PaginationTools.TryParsePageNumber(args.Parameters, 1, args.Player, out pageNumber))
+					return;
+				var lines = new List<string> { };
+				foreach (var gooda in config.All)
+				{
+					string total = $"* [c/ffa500:{gooda.DisplayName}]: ";
+					foreach (var item in gooda.IncludeItems)
+					{
+						total = $"{total} {ItemToTag(item)}";
+					}
+					total = $"{total} > [c/50c878:{Money.Parse(Convert.ToString(gooda.Price))}]";
 
-            }
-            foreach (var i1 in config.All)
-            {
-                
-                
-                if (medname.ToLower() == i1.DisplayName.ToLower())
-                {
-                    Find = i1;
-                    FindSuccess = true;
-                }
+					if (args.Player.Group.HasPermission(gooda.RequirePermission))
+					{
+						lines.Add(total);
+					}
+					else if (!config.HideUnavailableGoods)
+					{
+						string perm = args.Player.Group.HasPermission(gooda.RequirePermission) ? "[c/84d184:(Available)]" : "[c/ff0000:(Shortage)]";
+						lines.Add($"{perm} {total}");
+					}
+				}
+				PaginationTools.SendPage(args.Player, pageNumber, lines,
+										 new PaginationTools.Settings
+										 {
+											 HeaderFormat = "Menu ({0}/{1}):",
+											 FooterFormat = "Type {0}buy menu {{0}} for more goods.".SFormat(Commands.Specifier),
+											 HeaderTextColor = Color.Magenta,
+											 FooterTextColor = Color.ForestGreen,
+											 LineTextColor = Color.LightGray,
+											 MaxLinesPerPage = 3
+										 }
+										);
+				return;
+			}
+			var Find = new Goods();
+			bool FindSuccess = false;
+			string medname = args.Parameters[0];
+			for (int i = 1; i < args.Parameters.Count; i++)
+			{
+				medname = $"{medname} {args.Parameters[i]}";
+			}
 
-            }
+			foreach (var i1 in config.All)
+			{
+				if (medname.ToLower() == i1.DisplayName.ToLower())
+				{
+					Find = i1;
+					FindSuccess = true;
+				}
+			}
 
+			if ((!FindSuccess) || (!args.Player.Group.HasPermission(Find.RequirePermission) && config.HideUnavailableGoods))
+			{
+				args.Player.SendErrorMessage("Can't find a good with given name. Type [c/ffd700:/buy menu] for list.");
+				return;
+			}
+			if (!args.Player.Group.HasPermission(Find.RequirePermission))
+			{
+				args.Player.SendErrorMessage("There is a shortage! Why not try another goods?");
+				return;
+			}
 
+			var UsernameBankAccount = SEconomyPlugin.Instance.GetBankAccount(args.Player.Name);
+			var playeramount = UsernameBankAccount.Balance;
+			Money amount = -Find.Price;
+			Money amount2 = Find.Price;
+			var amount3 = Money.Parse(Convert.ToString(amount2));
+			var Journalpayment = BankAccountTransferOptions.AnnounceToReceiver;
 
-            if ((!FindSuccess) || (!args.Player.Group.HasPermission(Find.RequirePermission) && config.HideUnavailableGoods))
-            {
-                args.Player.SendErrorMessage("Can't find a good with given name. Type /buy menu for list.");
-                return;
-            }
-            if (!args.Player.Group.HasPermission(Find.RequirePermission))
-            {
-                args.Player.SendErrorMessage("There is a shortage! Why not try another goods?");
-                return;
-            }
+			if (args.Player == null || UsernameBankAccount == null)
+			{
+				args.Player.SendErrorMessage("Can't find the account for {0}.", args.Player.Name);
+				return;
+			}
 
+			if (playeramount < amount2)
+			{
+				args.Player.SendErrorMessage($"The price of [c/ffa500:{Find.DisplayName}] is [c/50c878:{Money.Parse(Convert.ToString(Find.Price))}], but you only have [c/ff0000:{UsernameBankAccount.Balance}] in your account.");
+				return;
+			}
 
+			if (!args.Player.InventorySlotAvailable)
+			{
+				args.Player.SendErrorMessage("Your inventory is full.");
+				return;
+			}
 
-            var UsernameBankAccount = SEconomyPlugin.Instance.GetBankAccount(args.Player.Name);
-            var playeramount = UsernameBankAccount.Balance;
-            Money amount = -Find.Price;
-            Money amount2 = Find.Price;
-            var amount3 = Wolfje.Plugins.SEconomy.Money.Parse(Convert.ToString(amount2));
-            var Journalpayment = Wolfje.Plugins.SEconomy.Journal.BankAccountTransferOptions.AnnounceToSender;
+			SEconomyPlugin.Instance.WorldAccount.TransferToAsync(UsernameBankAccount, amount,
+																Journalpayment, string.Format("Pay {0} to shop", amount2),
+																 string.Format("Buying " + Find.DisplayName));
+			args.Player.SendSuccessMessage("You have paid [c/50c878{0}] to buy [c/ffa500:{1}].", amount2, Find.DisplayName);
+			TShock.Log.ConsoleInfo("{0} has paid {2} to buy {1}.", args.Player.Name, Find.DisplayName, amount2);
+			foreach (var item in Find.IncludeItems)
+			{
+				var q = new Item();
+				q.netDefaults(item.netID);
+				q.stack = item.stack;
+				q.Prefix(item.prefix);
+				args.Player.GiveItemCheck(q.type, q.Name, q.width, q.height, q.stack, q.prefix);
+			}
+			/* Debug? */ /*var num = */
+			ShopDB.Query("INSERT INTO ShopHistory (Time, Account, ItemName, WorldID, price) VALUES (@0, @1, @2, @3, @4);", DateTime.Now, args.Player.Name, Find.DisplayName, Main.worldID, amount3);
+		}
 
-            
-
-            if (args.Player == null || UsernameBankAccount == null)
-            {
-                args.Player.SendErrorMessage("Can't find the account for {0}.", args.Player.Name);
-                return;
-            }
-
-            if (playeramount < amount2)
-            {
-                args.Player.SendErrorMessage("The price of " + Find.DisplayName + " is " + Wolfje.Plugins.SEconomy.Money.Parse(Convert.ToString(Find.Price)) + " , but you only have " + UsernameBankAccount.Balance + " in your account.");
-                return;
-            }
-
-
-
-            if (!args.Player.InventorySlotAvailable)
-            {
-                args.Player.SendErrorMessage("Your inventory is full.");
-                return;
-            }
-
-
-
-            SEconomyPlugin.Instance.WorldAccount.TransferToAsync(UsernameBankAccount, amount,
-                                                                 Journalpayment, string.Format("Pay {0} to shop", amount2),
-                                                                 string.Format("Buying " + Find.DisplayName));
-            args.Player.SendSuccessMessage("You have paid {0} to buy {1}.", amount2, Find.DisplayName);
-            TShock.Log.ConsoleInfo("{0} has paid {2} to buy {1}.", args.Player.Name, Find.DisplayName, amount2);
-            foreach (var item in Find.IncludeItems)
-            {
-                var q = new Item();
-                q.netDefaults(item.netID);
-                q.stack = item.stack;
-                q.Prefix(item.prefix);
-                args.Player.GiveItemCheck(q.type, q.Name, q.width, q.height, q.stack, q.prefix);
-            }
-            var num = ShopDB.Query("INSERT INTO ShopHistory (Time, Account, ItemName, WorldID, price) VALUES (@0, @1, @2, @3, @4);", DateTime.Now, args.Player.Name, Find.DisplayName, Main.worldID, amount3);
-           
-
-        }
-
-
-
-
-
-        private void CreateConfig()
+		private void CreateConfig()
 		{
 			string filepath = Path.Combine(TShock.SavePath, "Shop.json");
 			try
@@ -252,9 +231,9 @@ namespace Shop
 						{
 							var configString = sr.ReadToEnd();
 							config = JsonConvert.DeserializeObject<Config>(configString);
-							foreach (var element in config.All) 
+							foreach (var element in config.All)
 							{
-								foreach (var element2 in element.IncludeItems) 
+								foreach (var element2 in element.IncludeItems)
 								{
 									element2.Full();
 								}
@@ -289,14 +268,14 @@ namespace Shop
 	}
 	public class Config
 	{
-		public List<Goods> All;
 		public bool HideUnavailableGoods;
+		public List<Goods> All;
 		public Config()
-		{}
+		{ }
 		public Config(int a)
 		{
 			HideUnavailableGoods = true;
-			All = new List<Goods>{new Goods(1), new Goods(2)};
+			All = new List<Goods> { new Goods(1), new Goods(2) };
 		}
 	}
 	public class Goods
@@ -304,8 +283,8 @@ namespace Shop
 		public string DisplayName = "";
 		public string RequirePermission = "";
 		public int Price = 0;
-		public List<SimpleItem> IncludeItems = new List<SimpleItem>{};
-		public Goods() {}
+		public List<SimpleItem> IncludeItems = new List<SimpleItem> { };
+		public Goods() { }
 		public Goods(int a)
 		{
 			if (a == 1)
@@ -316,7 +295,7 @@ namespace Shop
 				DisplayName = "ExampleNebula";
 				RequirePermission = "shop.buy";
 				Price = 500000;
-				IncludeItems = new List<SimpleItem>{i1, i2, i3};
+				IncludeItems = new List<SimpleItem> { i1, i2, i3 };
 			}
 			if (a == 2)
 			{
@@ -335,7 +314,7 @@ namespace Shop
 		public int stack = 1;
 		public int prefix = 0;
 		public string name = "";
-		public SimpleItem() {}
+		public SimpleItem() { }
 		public SimpleItem(int a)
 		{
 			this.name = TShockAPI.Utils.Instance.GetItemByIdOrName(a.ToString())[0].Name;
@@ -345,8 +324,8 @@ namespace Shop
 		{
 			this.netID = TShockAPI.Utils.Instance.GetItemByIdOrName((netID != 0) ? netID.ToString() : name)[0].type;
 			this.name = TShockAPI.Utils.Instance.GetItemByIdOrName(netID.ToString())[0].Name;
-			this.stack = this.stack;
-			this.prefix = this.prefix;
+			//this.stack = this.stack;
+			//this.prefix = this.prefix;
 		}
 	}
 }
